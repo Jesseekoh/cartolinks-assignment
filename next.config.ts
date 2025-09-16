0a5d6051337a64e0f734b958433a847b3f317517
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-09-16 15:22:14 +0100

    chore(config): update image domains for better resource loading

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['images.unsplash.com', 'github.com'],
+    // remotePatterns: [new URL('https://unsplash.com/photos/**')],
+  },
 };
 
 export default nextConfig;
